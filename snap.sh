577963cbfe660722cd43cc70ee401323fbd70e51
Jessica Tarra <jessictarra@gmail.com>
2021-08-10 16:13:27 -0300

    Add snap list file

---
+++ snap.sh
@@ -0,0 +1,15 @@
+android-studio           2020.3.1.22                 114    latest/stable    snapcrafters      classic
+core                     16-2.51.3                   11420  latest/stable    canonical✓        core
+core18                   20210611                    2074   latest/stable    canonical✓        base
+core20                   20210702                    1081   latest/stable    canonical✓        base
+dbeaver-ce               21.1.3.202107181810         138    latest/stable    dbeaver-corp      -
+gnome-3-28-1804          3.28.0-19-g98f9e67.98f9e67  161    latest/stable    canonical✓        -
+gnome-3-34-1804          0+git.3556cb3               72     latest/stable/…  canonical✓        -
+gtk-common-themes        0.1-52-gb92ac40             1515   latest/stable/…  canonical✓        -
+insomnia                 2021.4.1                    144    latest/stable    gschier1990       -
+intellij-idea-community  2021.2                      315    latest/stable    jetbrains✓        classic
+kotlin                   1.5.21                      60     latest/stable    jetbrains✓        classic
+postman                  7.36.5                      133    latest/stable    postman-inc✓      -
+snap-store               3.38.0-64-g23c4c77          547    latest/stable/…  canonical✓        -
+snapd                    2.51.3                      12704  latest/stable    canonical✓        snapd
+telegram-desktop         2.9.0                       2956   latest/stable    telegram.desktop  
