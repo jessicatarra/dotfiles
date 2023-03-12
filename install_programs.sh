3018aa1939ca5dc8f588cc525b09d2f57375940b
Jessica Tarra <jessica@tarralva.com>
2023-03-12 13:46:19 -0300

    Update install programs script

--- install_programs.sh
+++ install_programs.sh
@@ -85,3 +85,6 @@
 #wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 #sudo apt install libu2f-udev
 #sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+#Install telegram
+sudo snap install telegram-desktop
