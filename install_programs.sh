de5a2d41d70c872915ef7be96b79658c6337210b
Jessica Tarra <jessica@tarralva.com>
2023-03-11 21:53:11 -0300

    Add linux install programs bash script

---
+++ install_programs.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+#Update and upgrade system
+#sudo apt update
+#sudo apt upgrade
+
+#Install neofetch
+#sudo apt install neofetch
+
+#Install vim
+#sudo apt install vim
+
+#Install Git
+#sudo apt install git
+
+#Install tilix 
+#sudo apt install tilix
+
+#Install ruby
+#sudo apt-get install ruby-full
+
+#Install node js and npm
+#sudo apt install nodejs
+#sudo apt install npm
+
+#Install snap
+#sudo apt install snapd
+
+#Install android studio 
+#sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
+#TODO: best way to automate this step
+
+#Install curl
+#sudo apt install curl
+
+#Install zsh
+#sudo apt install zsh
+
+#Zsh
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#NVM
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+#VS CODE
+#sudo snap install code --classic
+
+#Install Java
+#sudo apt install default-jre
+#sudo apt install default-jdk
+#java --version
+
+#Homebrew
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zshrc
+#sudo apt-get install build-essential
+
+#Install bitrise via brew
+#brew install bitrise
+
+#Install Github CLI via brew
+#brew install gh
+
+# Login to Github
+#gh auth login
+
+#Install zinit 
+#bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+
+# Source zshrc
+#source ~/.zshrc  
